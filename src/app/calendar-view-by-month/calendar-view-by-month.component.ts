fee32d3af10f996decf375271d0f8e7f3a036542
Michael Harootoonyan <michaelharootoonyan@yahoo.com>
2019-07-13 14:15:53 -0700

    injected calendarService dependency to calendar view by month component

--- src/app/calendar-view-by-month/calendar-view-by-month.component.ts
+++ src/app/calendar-view-by-month/calendar-view-by-month.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarEvent } from '../calendar-event';
+import { CalendarService } from '../calendar.service';
 
 @Component({
   selector: 'app-calendar-view-by-month',
@@ -14,12 +16,38 @@ export class CalendarViewByMonthComponent implements OnInit {
   numOfDays: number;
   // #weeks-placeholder
   weeksPlaceHolder: HTMLElement; 
+  calendarEvents: CalendarEvent[];
+  filteredCalendarEvents: CalendarEvent[];
+  eventDays: boolean[] = [false, false, false, false, false, false, false, false,
+    false, false, false, false,false, false, false, false,
+    false, false, false, false,false, false, false, false,
+    false, false, false, false,false, false, false, false];
 
 
   DAY_NAMES: string[]   = [ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
+  constructor(private calendarService:CalendarService) { }
 
-  constructor() { }
+  getCalendarEvents(): void {
+    this.calendarEvents = this.calendarService.getCalendarEvents();
+    this.month = this.month.charAt(0).toUpperCase() + this.month.slice(1);
+
+    this.calendarEvents.forEach( calendarEvent => {
+      console.log(calendarEvent);
+
+      if ( calendarEvent.starts.indexOf(this.month) > -1 && 
+           calendarEvent.starts.indexOf(this.year+"") > -1)
+      {
+        // this.filteredCalendarEvents.push(calendarEvent);
+        this.eventDays[parseInt(calendarEvent.starts.split(" ")[2])] = true;
+
+      }
+    });
+
+    // this.filteredCalendarEvents.forEach( calendarEvent => {
+    //   this.eventDays[parseInt(calendarEvent.starts.split(" ")[2])] = true;
+    // });
+  }
 
   initCalendarProperties(): void {
     var str = window.location + "";
@@ -35,6 +63,7 @@ export class CalendarViewByMonthComponent implements OnInit {
       this.dayOffset = parseInt(m[2]); // 0-6
       this.year      = parseInt(m[3]); // 2019
       this.dayCounter = 1;
+      this.getCalendarEvents();
     }
   }
 
@@ -42,6 +71,7 @@ export class CalendarViewByMonthComponent implements OnInit {
     this.initCalendarProperties();
     // console.log(this.month+"-"+this.numOfDays+"-"+this.dayOffset);
   }
+
   // will not throw any error because it will render after template loading
   ngAfterViewInit() {
     this.weeksPlaceHolder = document.getElementById('weeks-placeholder');
@@ -54,11 +84,13 @@ export class CalendarViewByMonthComponent implements OnInit {
    * This accounts for the offset depending on what day of the week the month starts
    */
   generateFirstWeekRow(): void {
+    var flagEventUnderline = false;
     var divFirstRowTextCenter = document.createElement('div'),
         divRowWeekUnderline   = document.createElement('div');
 
     divFirstRowTextCenter.className = "row text-center";
     divRowWeekUnderline.className   = "row week-underline";
+    
     /*
             <div class="row text-center">
                 <div class="col day-digits"></div>
@@ -74,12 +106,23 @@ export class CalendarViewByMonthComponent implements OnInit {
     for (var i = 0; i < 7; i++) {
       var divColDayDigits = document.createElement('div');
       divColDayDigits.className = "col day-digits";
+
       if ( i >= this.dayOffset ) {
         divColDayDigits.innerHTML = ""+this.dayCounter;
         this.dayCounter++;
+        // now is there an event?
+        if ( this.eventDays[this.dayCounter] ) {
+          flagEventUnderline = true;
+          divColDayDigits.innerHTML += '<br><svg class=\"event\" xmlns=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"20\" viewBox=\"0 0 20 20\"><path id=\"ic_radio_button_checked_24px\" d=\"M12,7a5,5,0,1,0,5,5A5,5,0,0,0,12,7Zm0-5A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm0,18a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z\" transform=\"translate(-2 -2)\"/></svg>'
+        }
       }
       divFirstRowTextCenter.appendChild(divColDayDigits);
     }
+    // add event css if there is an event any day of the week
+    if (flagEventUnderline) {
+      divRowWeekUnderline.className += " event";
+    }
+
     this.weeksPlaceHolder.appendChild(divFirstRowTextCenter);
     this.weeksPlaceHolder.appendChild(divRowWeekUnderline);
 
@@ -89,6 +132,7 @@ export class CalendarViewByMonthComponent implements OnInit {
     var n = 1 + this.numOfDays;
 
     while ( this.dayCounter < n ) {
+      var flagEventUnderline = false;
       var divFirstRowTextCenter = document.createElement('div'),
         divRowWeekUnderline   = document.createElement('div');
 
@@ -101,16 +145,25 @@ export class CalendarViewByMonthComponent implements OnInit {
             divColDayDigits.className = "col day-digits";
         if (this.dayCounter < n) {
           divColDayDigits.innerHTML = ""+this.dayCounter;
+          // now is there an event?
+          if ( this.eventDays[this.dayCounter] ) {
+            flagEventUnderline = true;
+            divColDayDigits.innerHTML += '<br><svg class=\"event\" xmlns=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"20\" viewBox=\"0 0 20 20\"><path id=\"ic_radio_button_checked_24px\" d=\"M12,7a5,5,0,1,0,5,5A5,5,0,0,0,12,7Zm0-5A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm0,18a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z\" transform=\"translate(-2 -2)\"/></svg>'
+          }
           this.dayCounter++;
         }
         divFirstRowTextCenter.appendChild(divColDayDigits);
+      }
+      // add event css if there is none on an event any day of the week
+      if (flagEventUnderline)  {
+        divRowWeekUnderline.className = "row week-underline event";
       }
       this.weeksPlaceHolder.appendChild(divFirstRowTextCenter);
       this.weeksPlaceHolder.appendChild(divRowWeekUnderline);
 
       
 
-    }
+    }// end of while loop
     
   }
 
