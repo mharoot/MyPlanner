3b174ee661056457edb64c6824f6e2c35a3725fa
Michael Harootoonyan <michaelharootoonyan@yahoo.com>
2019-07-13 15:07:12 -0700

    added more mock events to test

--- src/app/mock-calendar-events.ts
+++ src/app/mock-calendar-events.ts
@@ -7,6 +7,9 @@ import { CalendarEvent } from './calendar-event';
 
 export const MOCK_CALENDAR_EVENTS: CalendarEvent[] = [
   { id: 11, alert:'5 minutes before', allDay: false, ends:'Tue June 04 2019 10:30am', location: 'Home', notes:"Interview w/ Reid Gormmley",repeat: 'Never', starts: 'Tue June 04 2019 10:00am', title: 'Robert Half Recruiter', travelTime:'None', type: 'Phone Interview', url: null },
-  { id: 12, alert:'15 minutes before', allDay: false, ends:'Thu July 04 2019 10:30am', location: 'Home', notes:"Interview w/ Simon Grute",repeat: 'Never', starts: 'Thu July 04 2019 10:00am', title: 'Robert Half Recruiter', travelTime:'None', type: 'Phone Interview', url: null }
+  { id: 12, alert:'15 minutes before', allDay: false, ends:'Thu July 04 2019 10:30am', location: 'Home', notes:"Interview w/ Simon Grute",repeat: 'Never', starts: 'Thu July 04 2019 10:00am', title: 'Robert Half Recruiter', travelTime:'None', type: 'Phone Interview', url: null },
+  { id: 13, alert:'10 minutes before', allDay: false, ends:'Fri June 14 2019 10:30am', location: 'Home', notes:"Interview w/ Chuck Phillips",repeat: 'Never', starts: 'Fri June 14 2019 10:00am', title: 'Robert Half Recruiter', travelTime:'None', type: 'Phone Interview', url: null },
+  { id: 14, alert:'30 minutes before', allDay: false, ends:'Mon June 24 2019 10:30am', location: 'Home', notes:"Interview w/ Anthony Hamilton",repeat: 'Never', starts: 'Mon June 24 2019 10:00am', title: 'Robert Half Recruiter', travelTime:'None', type: 'Phone Interview', url: null },
+  { id: 15, alert:'1 hour before', allDay: false, ends:'Sun July 14 2019 10:30am', location: 'Home', notes:"Interview w/ Jessica Lopez",repeat: 'Never', starts: 'Sun July 14 2019 10:00am', title: 'Robert Half Recruiter', travelTime:'None', type: 'Phone Interview', url: null }
 ];
 // Tues June 04 2019
