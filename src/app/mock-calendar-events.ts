38813bdc24e5d602a14411489a2d472b04bcdfd6
Michael Harootoonyan <michaelharootoonyan@yahoo.com>
2019-07-13 14:14:32 -0700

    mock data contains july 04 and june 04 of 2019

--- src/app/mock-calendar-events.ts
+++ src/app/mock-calendar-events.ts
@@ -6,6 +6,7 @@
 import { CalendarEvent } from './calendar-event';
 
 export const MOCK_CALENDAR_EVENTS: CalendarEvent[] = [
-  { id: 11, alert:'5 minutes before', allDay: false, ends:'Tues June 04 2019 10:30am', location: 'Home', notes:"Interview w/ Reid Gormmley",repeat: 'Never', starts: 'Tues June 04 2019 10:00am', title: 'Robert Half Recruiter', travelTime:'None', type: 'Phone Interview', url: null },
+  { id: 11, alert:'5 minutes before', allDay: false, ends:'Tue June 04 2019 10:30am', location: 'Home', notes:"Interview w/ Reid Gormmley",repeat: 'Never', starts: 'Tue June 04 2019 10:00am', title: 'Robert Half Recruiter', travelTime:'None', type: 'Phone Interview', url: null },
+  { id: 12, alert:'15 minutes before', allDay: false, ends:'Thu July 04 2019 10:30am', location: 'Home', notes:"Interview w/ Simon Grute",repeat: 'Never', starts: 'Thu July 04 2019 10:00am', title: 'Robert Half Recruiter', travelTime:'None', type: 'Phone Interview', url: null }
 ];
 // Tues June 04 2019
